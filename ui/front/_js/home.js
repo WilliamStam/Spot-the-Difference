b74c4b02f5b2864756025baa98cb20263727ce49
William Stam <william@impreshin.com>
2016-10-05 13:14:54 +0200

    latest

--- ui/front/_js/home.js
+++ ui/front/_js/home.js
@@ -1,11 +1,70 @@
+var stats = {
+	
+	"clicks":0,
+	"hit":0,
+	"miss":0,
+	"accuracy":0,
+	"total":0,
+	"found":0,
+}
 $(document).ready(function () {
 	
-	
+	$(document).on("click",".canvas-area",function(e){
+		if($(e.target).is('.difference')){
+			stats.hit = stats.hit + 1;
+		} else {
+			stats.miss = 	stats.miss + 1;
+			
+		}
+		stats.clicks = 	stats.clicks + 1;
+		diffCalculator();
+	});
+	$(document).on("click",".difference",function(){
+		var $this = $(this);
+		var dif = $this.attr("data-diff");
+		
+		$(".difference-"+dif).addClass("active");
+		
+	});
 	
 	getData();
 	
 });
-
+function diffCalculator(){
+	updateStats();
+	
+	if (stats.found){
+		var percent = (stats.found / stats.total)*100;
+		
+		
+		$("#footer-progress").find(".progress-bar").text(percent.toFixed(0) + "%").css("width",percent+"%");
+	
+		if (stats.found == stats.total){
+			toastr["success"]("You found all the differences!", "Success");
+		}
+	}
+	
+	
+	
+	
+}
+function updateStats(){
+	stats.found = $(".difference.active",$("#canvasdiv-image_left")).length;
+	stats.total = $(".difference",$("#canvasdiv-image_left")).length;
+	
+	$("#stats-clicks").text(stats.clicks);
+	$("#stats-hit").text(stats.hit);
+	$("#stats-miss").text(stats.miss);
+	
+	var accuracy = (stats.found / (stats.clicks - stats.hit))*100
+	accuracy = accuracy.toFixed(2);
+	$("#stats-accuracy").text(accuracy);
+	
+	$(".found-count").html(stats.found)
+	
+	
+	console.log(stats)
+}
 function getData(){
 	var ID = _ID;
 	var image_width = 0;
@@ -34,16 +93,19 @@ function getData(){
 			var paper = Raphael($("#canvasdiv-"+id).get(0));
 			paper.setViewBox(0,0,data.canvas.width,data.canvas.height,true);
 			paper.setSize(data.file.width,data.file.height);
-			
+			var id=1;
 			for (var i in data.slide.data){
-				paper.path(data.slide.data[i]).attr({"type":"path","stroke":"none","fill":"#ffffff","stroke-opacity":0,"fill-opacity":0.4,});
+				var obj = paper.path(data.slide.data[i]).attr({"type":"path","stroke":"none","fill":"#ffffff","stroke-opacity":0,"fill-opacity":0.4,});
+				obj.node.setAttribute("class","difference difference-" + id);
+				obj.node.setAttribute("data-diff","" + id);
+				id++
 			}
 			
 
 			
 			
 		});
-		
+		diffCalculator();
 	//	$("#image_left")
 		
 		//$("#content-area").jqotesub($("#template-slide"), data);
